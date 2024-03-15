acf43c6ab8ef15b805ee491ea757194902fe11c4
DaCatInDaHat <volodymyrkuznetsov87@gmail.com>
2024-03-15 13:25:20 +0200

    deploy to firebase

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { initializeApp } from "firebase/app";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+initializeApp({
+  apiKey: "AIzaSyDalk2hokwJJHPjvMhjrmTfQC1v2zlyNF4",
+  authDomain: "digital-business-card-13265.firebaseapp.com",
+  projectId: "digital-business-card-13265",
+  storageBucket: "digital-business-card-13265.appspot.com",
+  messagingSenderId: "930839925238",
+  appId: "1:930839925238:web:aa880dd20900bd941ab7d7",
+  measurementId: "G-FE7ECL7VHV"
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
